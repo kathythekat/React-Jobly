ae7ebfe1e62093a3867af6aaff2d478beab2169c
Jessica <jessica@Jessicas-MacBook-Pro.local>
2021-04-13 10:59:59 -0700

    add joblyapi file that handles ajax requests for front end

---
+++ src/JoblyAPI.js
@@ -0,0 +1,72 @@
+import axios from "axios";
+
+const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
+
+/** API Class.
+ *
+ * Static class tying together methods used to get/send to to the API.
+ * There shouldn't be any frontend-specific stuff here, and there shouldn't
+ * be any API-aware stuff elsewhere in the frontend.
+ *
+ */
+
+class JoblyApi {
+  // the token for interactive with the API will be stored here.
+  static token;
+
+  static async request(endpoint, data = {}, method = "get") {
+    console.debug("API Call:", endpoint, data, method);
+
+    const url = `${BASE_URL}/${endpoint}`;
+    const headers = { Authorization: `Bearer ${JoblyApi.token}` };
+    const params = method === "get" ? data : {};
+
+    try {
+      return (await axios({ url, method, data, params, headers })).data;
+    } catch (err) {
+      console.error("API Error:", err.response);
+      let message = err.response.data.error.message;
+      throw Array.isArray(message) ? message : [message];
+    }
+  }
+
+  // Individual API routes
+
+  /** Get details on a company by handle. */
+
+  static async getCompany(handle) {
+    let res = await this.request(`companies/${handle}`);
+    return res.company;
+  }
+
+  static async getAllCompanies() {
+    let res = await this.request("/companies");
+    return res.companiesRes.rows;
+  }
+
+  static async getAllJobs() {
+    let res = await this.request("/jobs");
+    return res.jobsRes.rows;
+  }
+
+  static async getJob(id) {
+    let res = await this.request(`/jobs/${id}`);
+    return res.job;
+  }
+
+  static async register(data) {
+    let res = await this.request("auth/register", data, "post")
+    return res.user;
+  }
+
+  static async update(username, data) {
+    let res = await this.request(`/users/${username}`, data, "patch")
+  }
+  // obviously, you'll add a lot here ...
+}
+
+// for now, put token ("testuser" / "password" on class)
+JoblyApi.token =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZ" +
+  "SI6InRlc3R1c2VyIiwiaXNBZG1pbiI6ZmFsc2UsImlhdCI6MTU5ODE1OTI1OX0." +
+  "FtrMwBQwe6Ue-glIFgz_Nf8XxRT2YecFCiSpYL0fCXc";
