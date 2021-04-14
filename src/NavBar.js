ea8e5aa2a527ecdbf6d5d37863db6bd4068abf0f
Jessica <jessica@Jessicas-MacBook-Pro.local>
2021-04-14 12:02:46 -0700

    add onclick to logout on navbar to trigger logout function. refactor navbar to bootstrap navbar

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,15 +1,58 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
+import "./NavBar.css"
 
-function NavBar() {
+function NavBar({ token, logout }) {
   return (
-    <div>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/companies">Companies</NavLink>
-      <NavLink to="/jobs">Jobs</NavLink>
-      <NavLink to="/profile">Profile</NavLink>
-      <NavLink to="/login">Login</NavLink>
-      <NavLink to="/signup">Signup</NavLink>
-    </div>
+    <>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <Link to="/" className="navbar-brand mr-auto">
+          Jobly
+        </Link>
+        {token && (
+          <nav className="navbar-expand navbar" id="navbarNav">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item mr-4">
+                <NavLink to="/companies" className="nav-link">
+                  Companies
+                </NavLink>
+              </li>
+              <li className="nav-item mr-4">
+                <NavLink to="/jobs" className="nav-link">
+                  Jobs
+                </NavLink>
+              </li>
+              <li className="nav-item mr-4">
+                <NavLink to="/profile" className="nav-link">
+                  Profile
+                </NavLink>
+              </li>
+              <li className="nav-item mr-4">
+                <NavLink to="/logout" className="nav-link disabled">
+                  Logout
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+        )}
+
+        {!token && (
+          <nav className="navbar navbar-expand" id="navbarNav">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item mr-4">
+                <NavLink to="/login" className="nav-link">
+                  Login
+                </NavLink>
+              </li>
+              <li className="nav-item mr-4">
+                <NavLink to="/signup" className="nav-link">
+                  Signup
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+        )}
+      </nav>
+    </>
   );
 }
 
